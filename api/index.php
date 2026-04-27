93a31e2e633eb399d19d7f2752f33bc36034b59c
unknown <Yoyoaya56367@gmail.com>
2026-04-27 12:21:18 +0100

    ff

--- api/index.php
+++ api/index.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-                <?php
-        // put your code here
-       
-/**
- * Fichier d'entrée pour le projet E-Fruit
- * Redirige l'utilisateur vers la page d'authentification
- */
-
-// Redirection vers acc.php
+<?php
+// Redirection immédiate sans aucun contenu HTML avant
 header("Location: acc.php");
 exit();
 ?>
-    </body>
-</html>
